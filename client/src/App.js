1a9afe0545688a9405ed25f285c3b5c7f63148a5
amieltahir <amieltahir@gmail.com>
2023-01-19 22:41:19 -0500

    TRS

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 // eslint-disable-next-line
 import react from "react"
+import Navbar from "./components/Navbar/navbar";
 
 const App = ()=>{
   return (
     <>
-<h1> Hello react Js, Adding navbar</h1>
-<h1> Different</h1>
-<h1> Mobile View</h1>
+<Navbar />
 </>
   
   );
---
+++ client/src/components/Navbar/navbar.js
@@ -0,0 +1,22 @@
+import react from "react";
+import "./navbar.css";
+const Navbar = ()=>{
+    return(
+    <nav className="navbar-main">
+        <u1>
+            <li className="nav-item"> Home </li>
+            <li className="nav-item"> Technology Route Services INC. - TRS  </li>
+            <li className="nav-item"> Cloud Deployments  </li>
+            </u1>
+            </nav>
+
+        
+        
+    );
+};
+
+export default Navbar;
+
+
+
+
